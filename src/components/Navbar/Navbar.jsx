c928d58de7b642c3c1a78ffb8c632a2495bef4df
Pulkit <jauharipulkit17@gmail.com>
2024-06-03 23:46:19 +0530

    changed logo from tomato to quickbite

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   const [menu , setMenu] = useState("home");
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" />
+        <img className='logo' src={assets.quickbite} alt="" />
         <ul className='navbar-menu'>
             <li onClick={()=>setMenu("home")}className={menu==="home"&&"active"}>home</li>
             <li onClick={()=>setMenu("menu")}className={menu==="menu"&&"active"}>menu</li>
